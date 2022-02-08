e66a9204b0eccb67dcc43515a492a289077bdfe7
Heitor <hsbicalho@gmail.com>
2022-02-08 18:59:53 -0300

    desafio 11 implementado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumbers) {
+  let min = Math.min(...arrayNumbers);
+  let max = Math.max(...arrayNumbers);
+  if (arrayNumbers.length !== 11) return 'Array com tamanho incorreto.';
+  if (max > 9)
+    return 'não é possível gerar um número de telefone com esses valores';
+  if (min < 0)
+    return 'não é possível gerar um número de telefone com esses valores';
+  for (let i = 0; i < arrayNumbers.length; i++) {
+    let cont = 0;
+    for (let j = 0; j < arrayNumbers.length; j++) {
+      if (arrayNumbers[i] === arrayNumbers[j]) cont++;
+    }
+    if (cont > 2)
+      return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return `(${arrayNumbers[0]}${arrayNumbers[1]}) ${arrayNumbers[2]}${arrayNumbers[3]}${arrayNumbers[4]}${arrayNumbers[5]}${arrayNumbers[6]}-${arrayNumbers[7]}${arrayNumbers[8]}${arrayNumbers[9]}${arrayNumbers[10]}`;
 }
 
 // Desafio 12
