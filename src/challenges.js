ca3a5612f1a0db80755e28e0a803461ca99091e7
Heitor <hsbicalho@gmail.com>
2022-02-08 17:50:32 -0300

    desafio 9 implementado

--- src/challenges.js
+++ src/challenges.js
@@ -69,18 +69,36 @@ function fizzBuzz(arrayNumbers) {
   return arrayString;
 }
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  string = string.replace(/a/g, '1');
+  string = string.replace(/e/g, '2');
+  string = string.replace(/i/g, '3');
+  string = string.replace(/o/g, '4');
+  string = string.replace(/u/g, '5');
+  return string;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  string = string.replace(/1/g, 'a');
+  string = string.replace(/2/g, 'e');
+  string = string.replace(/3/g, 'i');
+  string = string.replace(/4/g, 'o');
+  string = string.replace(/5/g, 'u');
+  return string;
 }
 
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
 
+function techList(arrayTech, name) {
+  arrayTech.sort();
+  for (const x in arrayTech) {
+    objeto[x] = {
+      tech: arrayTech[x],
+      name: name,
+    };
+    console.log(objeto);
+  }
+}
+techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'heitor');
 module.exports = {
   calcArea,
   catAndMouse,
